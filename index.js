18431cdcfdba27490a5e2c087df0ddde4a2ebb56
KwakuAldo <x.pac20032001@gmail.com>
2024-04-13 00:10:09 +0000

    fix issue with form element refusing to hide on submit

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-const submitBtn = document.getElementById('submit');
-const alertMsgEl = document.getElementById('alertMsg_El');
-const dismissBtn = document.getElementById('dismissAlert');
-const overlay = document.getElementById('overlay');
+document.addEventListener('DOMContentLoaded', function() {
+    const formContainer = document.getElementById('form_container');
+    const submitBtn = document.getElementById('submit');
+    const alertMsgEl = document.getElementById('alertMsg_El');
+    const dismissBtn = document.getElementById('dismissAlert');
 
 
-submitBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    alertMsgEl.classList.remove('hidden')
-    alertMsgEl.classList.add('flex')
-    alertMsgEl.style.zIndex = "11"
-    overlay.classList.remove('hidden')
-    let emailInput = document.getElementById('email')
-    let userEmail = document.getElementById('user_email')
-    userEmail.innerText = emailInput.value;
-    console.log(userEmail)
+    //switch between illustration sign up image depending on screen size
+    window.addEventListener('resize', function () {
+        const isMobile = window.innerWidth < 768;
+        if (isMobile) {
+            document.getElementById('illustration_img').src = 'images/illustration-sign-up-mobile.svg';
+        } else {
+            document.getElementById('illustration_img').src = 'images/illustration-sign-up-desktop.svg';
+        }
+    })
+
+    submitBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        formContainer.classList.remove('md:flex');
+        formContainer.classList.add('hidden');
+        alertMsgEl.classList.remove('hidden');
+        alertMsgEl.classList.add('flex');
+        let emailInput = document.getElementById('email');
+        let userEmail = document.getElementById('user_email');
+        userEmail.innerText = emailInput.value;
+        console.log(userEmail.innerText);
+    })
+
+
+    dismissBtn.addEventListener('click', (e) => {
+        alertMsgEl.classList.remove('flex')
+        alertMsgEl.classList.add('hidden')
+        formContainer.classList.remove('hidden')
+        formContainer.classList.add('md:flex')
+    })
+
+
+    // Prompt user to enter valid email
+    // function validateEmail() {
+    //     const emailInput = document.getElementById('email')
+    // }
 })
-
-
-dismissBtn.addEventListener('click', (e) => {
-    alertMsgEl.classList.remove('flex')
-    alertMsgEl.classList.add('hidden')
-    overlay.classList.add('hidden')
-})
-
-//switch between illustration sign up image depending on screen size
-window.addEventListener('resize', function() {
-    const isMobile = window.innerWidth < 768;
-    if (isMobile) {
-        document.getElementById('illustration_img').src = 'images/illustration-sign-up-mobile.svg';
-    } else {
-        document.getElementById('illustration_img').src = 'images/illustration-sign-up-desktop.svg';
-    }
-})
-
-// Prompt user to enter valid email
-// function validateEmail() {
-//     const emailInput = document.getElementById('email')
-// }
